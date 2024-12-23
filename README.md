69ba4e78f4d1ecbcf1d8e8388f6bcb17affc7558
Chandra Shekhar <jmbd.group@gmail.com>
2024-12-23 17:34:43 +0530

    install axios and try to fetch data for login

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 - install MUI Core and Icons
 - npm install react-router-dom react-redux @reduxjs/toolkit
 - setup store and user slice
+- install mui material and icons
+- install react-router-dom
+- install axios and try to fetch data for login
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, TextField } from '@mui/material';
+import axios from 'axios';
 
 const LoginForm = () => {
+  const [loginForm, setLoginForm] = useState(true);
+  const [formData, setFormData] = useState({
+    identifier: '', // For mobile number or email
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+   try { 
+    const res = await axios.post("http://localhost:7777/api/v1/auth/login", formData,{
+      withCredentials:true
+    })
+
+   } catch (err) {
+    console.error(err)
+   }
+  };
+
+  const toggleForm =()=>{
+    setLoginForm(!loginForm);   
+  };
+
   return (
-    <div>LoginForm</div>
-  )
-}
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="w-full max-w-sm bg-white p-6 rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold text-center mb-6">{loginForm ? "Login" : "Signup"}</h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <TextField
+            label="Mobile No or Email"
+            variant="outlined"
+            fullWidth
+            name="identifier"
+            value={formData.identifier}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            className="bg-blue-500 hover:bg-blue-600 text-white"
+          >
+            Submit
+          </Button>
+          <div>
+            <p className="hover:text-blue-800" onClick={toggleForm} >{loginForm ? "Don't have an account ? Signup" : "Already have an account ?  Login"}
+              </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default LoginForm;
