f856929d3a5dd7b8e5f33551876e5dd9ead97fc4
Chandra Shekhar <jmbd.group@gmail.com>
2025-02-25 17:51:47 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { Provider } from "react-redux";
-import appStore from "./store/appStore";
 import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
-    <Provider store={appStore}>
       <AppRoutes />
-    </Provider>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
 import "./index.css";
+import appStore from "./store/appStore";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-    <App />
-);
+createRoot(document.getElementById('root')).render(
+    <Provider store={appStore}>
+        <App />
+      </Provider>
+  )
