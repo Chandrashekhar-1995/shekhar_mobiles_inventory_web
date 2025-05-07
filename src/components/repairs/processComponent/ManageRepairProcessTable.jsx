9481b1955fe47cc31eaafe056b16794b9f5aa032
Chandra Shekhar <jmbd.group@gmail.com>
2025-05-07 16:49:09 +0530

    Manbage repair process

--- src/components/repairs/processComponent/ManageRepairProcessTable.jsx
+++ src/components/repairs/processComponent/ManageRepairProcessTable.jsx
@@ -7,35 +7,36 @@ const ManageRepairProcessTable = () => {
 
     useFetchRepairProcesses();
     const processes = useSelector((store) => store.repairProcesses.allProcesses);
+    console.log("processes", processes);
+    
     return (
         <div className="overflow-x-auto">
             <table className="min-w-full border-collapse border border-gray-300">
                 <thead>
                     <tr className="bg-blue-500 text-white">
                         <th className="px-4 py-2">S.No.</th>
-                        <th className="px-4 py-2">Device </th>
                         <th className="px-4 py-2">Problem</th>
+                        <th className="px-4 py-2">Category</th>
+                        <th className="px-4 py-2">Device</th>
                         <th className="px-4 py-2">Process Name</th>
                         <th className="px-4 py-2">Status</th>
+                        <th className="px-4 py-2">Created By</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {processes && processes.map((p, index) => (
+                    {processes && processes?.map((p, index) => (
                         <tr key={p._id}className="odd:bg-white even:bg-gray-100">
                             <td rowSpan={p.length} className="border px-4 py-2">{index + 1}</td>
+                            <td rowSpan={p.length} className="border px-4 py-2">{p.fault.fault}</td>
+                            <td rowSpan={p.length} className="border px-4 py-2">{p.subFaults}</td>
                             <td rowSpan={p.length} className="border px-4 py-2">{p.deviceType}</td>
-                            <td rowSpan={p.length} className="border px-4 py-2">
-                                {/* update this link to if clik than show connected all repair */}
-                                <Link to={`/repair-process/update/${p._id}`} className="text-blue-500 hover:underline">
-                                    {p.fault}
-                                </Link>
-                            </td>
                             <td rowSpan={p.length} className="border px-4 py-2">
                                 <Link to={`/repair-process/update/${p._id}`} className="text-blue-500 hover:underline">
                                     {p.processName}
                                 </Link>
                             </td>
                                 <td rowSpan={p.length} className="border px-4 py-2">{p.isActive ? "Active" : "Deactive"}</td>
+                                <td rowSpan={p.length} className="border px-4 py-2">{p.createdBy.name}</td>
                         </tr>
                     ))}
                 </tbody>
