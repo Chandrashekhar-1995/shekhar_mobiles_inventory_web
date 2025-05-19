7765bdbad7835c32e746e91221301948bdb7f798
Chandra Shekhar <jmbd.group@gmail.com>
2025-05-19 12:33:09 +0530

    deploy

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,3 +1,3 @@
-// export const API_BASE_URL = "http://localhost:7777/api/v1/";
-
-export const API_BASE_URL = location.hostname === "localhost" ? "http://localhost:7777/api/v1" : "/api/v1"; 
+export const API_BASE_URL = location.hostname === "localhost"
+  ? "http://localhost:7777/api/v1"
+  : "http://13.61.24.184 :7777/api/v1";
